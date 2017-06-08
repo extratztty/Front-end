8f5d7d6c33d5d0c5159aec41a15ba2cf139723c6
extratztty <1539117070@qq.com>
2017-06-08 16:59:08 +0800

    Front-end

--- jstraining/demos/react-lifecycle-demo/jquery.js
+++ jstraining/demos/react-lifecycle-demo/jquery.js
@@ -1928,7 +1928,7 @@ Expr = Sizzle.selectors = {
 		},
 
 		"checked": function( elem ) {
-			// In CSS3, :checked should return both checked and selected elements
+			// In css3, :checked should return both checked and selected elements
 			// http://www.w3.org/TR/2011/REC-css3-selectors-20110929/#checked
 			var nodeName = elem.nodeName.toLowerCase();
 			return (nodeName === "input" && !!elem.checked) || (nodeName === "option" && !!elem.selected);
--- jstraining/demos/simple-app-demo/bundle.js
+++ jstraining/demos/simple-app-demo/bundle.js
@@ -2107,7 +2107,7 @@
 			"disabled": createDisabledPseudo( true ),
 
 			"checked": function( elem ) {
-				// In CSS3, :checked should return both checked and selected elements
+				// In css3, :checked should return both checked and selected elements
 				// http://www.w3.org/TR/2011/REC-css3-selectors-20110929/#checked
 				var nodeName = elem.nodeName.toLowerCase();
 				return (nodeName === "input" && !!elem.checked) || (nodeName === "option" && !!elem.selected);
